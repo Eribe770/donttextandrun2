6fbfaa5f6421177329537d1c2f1fe918ea13fabd
Erik Bennerhed <Erik Bennerhed>
2017-05-06 18:42:13 +0200

    Fixat timer

---
+++ Assets/Scripts/Level/Countdown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour {
+    public float TimeLimit;
+    private float TimeLeft = 100f;
+    private Text Counter;
+	// Use this for initialization
+	void Start () {
+	}
+
+    private void FixedUpdate()
+    {
+        Debug.Log("but why");
+        TimeLeft -= Time.fixedDeltaTime;
+        GetComponent<Text>().text = "Time left: " + TimeLeft;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Level/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 10.0f;
+
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime;
+    }
+
+    void OnGUI()
+    {
+        if (timeRemaining > 0)
+        {
+            GUI.Label(new Rect(0, 0, 200, 100),
+                        "Time Remaining : " + (int)timeRemaining);
+        }
+        else
+        {
+            GUI.Label(new Rect(00, 00, 200, 100), "Time's Up");
+        }
+    }
+}
